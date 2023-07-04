6bff371370f38508234823c0fb2f0617ba43bb81
Shadmehr Khan <srk169@scarletmail.rutgers.edu>
2023-07-03 21:29:51 -0400

    finished server

--- Project_0/Server.py
+++ Project_0/Server.py
@@ -1,28 +1,26 @@
 import socket
+from sys import argv
 import argparse
+
 
 #Used the parser from the provided Client.py, with a minor adjustment
 parser=argparse.ArgumentParser(description="""This is the corresponding server program""")
-parser.add_argument('-f', type=str, help='This is the source file for the strings to reverse', default='source_strings.txt',action='store', dest='in_file')
-parser.add_argument('-o', type=str, help='This is the destination file for the reversed strings', default='results.txt',action='store', dest='out_file')
+#parser.add_argument('-f', type=str, help='This is the source file for the strings to reverse', default='source_strings.txt',action='store', dest='in_file')
+#parser.add_argument('-o', type=str, help='This is the destination file for the reversed strings', default='results.txt',action='store', dest='out_file')
 parser.add_argument('port', type=int, help='This is the port to bind the server on',action='store')
 args = parser.parse_args()
-
+print(args.port)
 #Creates the server socket.
 server_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 #Binds the server socket to indicated port and sets the server location to an empty string, 
 # which lets it listen for all networks rather than just a specific ip or local host.
-server_addr = ('', args.port)
+server_addr = (socket.gethostname(), args.port)
+print(server_addr)
 server_sock.bind(server_addr)
 
 #Listens for incoming connections from the client computer.
-server_sock.listen(1)
-
-#A function for repeatedly switching lowercase to uppercase and vice versa for each string sent from client.
-def switched_string(input_string):
-    switched_string = input_string.swapcase()
-    return switched_string
+server_sock.listen(5)
 
 while True:
     print("Waiting to connect...")
@@ -31,22 +29,15 @@ while True:
     client_sock, client_addr = server_sock.accept()
     print("Connected to: ", client_addr)
 
-    #Opens the original and result files.
-    with open (args.out_file, 'wb') as write_file:
-        for line in open(args.in_file, 'rb'):
-            #This is the data received from client.
-            data = client_sock.recv(512)
-
-            if not data:
-                break
-
-            #Switches the strings.
-            switched_data = switched_string(data.decode())
-            write_file.write(switched_data.encode() + b'\n')
-
-            #Sends the switched data back to client.
-            client_sock.sendall(switched_data.encode())
-
+    while True:
+        #reads data
+        data = client_sock.recv(512)
+        #breaks if end of file
+        if not data:
+            break
+        modified_data = data.decode("utf-8").swapcase()
+        #sends reversed case to client
+        client_sock.send(bytes(modified_data,'utf-8'))
     #Closes the client once finished transferring data.
     client_sock.close()
 
