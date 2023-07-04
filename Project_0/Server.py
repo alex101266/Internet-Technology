cceb554dbd1c59e05851da9690fe30641b3a2175
Shadmehr Khan <srk169@scarletmail.rutgers.edu>
2023-07-03 21:39:50 -0400

    server works

--- Project_0/Server.py
+++ Project_0/Server.py
@@ -5,18 +5,14 @@ import argparse
 
 #Used the parser from the provided Client.py, with a minor adjustment
 parser=argparse.ArgumentParser(description="""This is the corresponding server program""")
-#parser.add_argument('-f', type=str, help='This is the source file for the strings to reverse', default='source_strings.txt',action='store', dest='in_file')
-#parser.add_argument('-o', type=str, help='This is the destination file for the reversed strings', default='results.txt',action='store', dest='out_file')
 parser.add_argument('port', type=int, help='This is the port to bind the server on',action='store')
 args = parser.parse_args()
-print(args.port)
 #Creates the server socket.
 server_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 #Binds the server socket to indicated port and sets the server location to an empty string, 
 # which lets it listen for all networks rather than just a specific ip or local host.
 server_addr = (socket.gethostname(), args.port)
-print(server_addr)
 server_sock.bind(server_addr)
 
 #Listens for incoming connections from the client computer.
