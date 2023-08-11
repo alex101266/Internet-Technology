a5203c3aaa0b532cb2423988a4f8eb293519bf13
alex101266 <123588114+alex101266@users.noreply.github.com>
2023-08-10 20:11:27 -0400

    added readme

--- Project_2/TCP_socket.py
+++ Project_2/TCP_socket.py
@@ -20,28 +20,30 @@ class TCP_Connection_Final(TCP_Connection):
 		#NOTE: this code can send one packet, but should never send more than one packet
 		for packet in packets:
 			#Check sequence number
-			segment_length = len(packet.data)
+			segment_len = len(packet.data)
 			#Send ACK if new data receieved
-			if segment_length > 0:
-				self.RCV.ACK = packet.SEQ + segment_length
+			if segment_len > 0:
+				self.RCV.ACK = packet.SEQ + segment_len
+				self.RCV.WND -= segment_len
 				self._packetize_and_send(self.SND.NXT)
-
+				
+		
+	
 	def send_data(self, window_timeout = False, RTO_timeout = False):
 		#put code to send a single packet of data here
 		#note that this code does not always need to send data, only if TCP policy thinks it makes sense
 		#if there is any data to send, i.e. we have data we have not sent and we are allowed to send by our
 		#congestion and flow control windows, then send one packet of that data
 
-		#Check how much data can be sent
+		#Check how much data can be sent.
 		size = min(self.SND.MSS, self.SND.WND, self.congestion_window)
 		start = self.SND.NXT - self.SND.UNA
 		end = start + size
 
-		#Get the needed segment from send_buff
+		#Get the needed segment from send_buff.
 		segment = self.send_buff[start:end]
-		#print("*****",start,size,end,segment)
 
-		#Check push flag, send one packet
+		#Check push flag, send one packet.
 		flag = False
 		data_to_send = bytearray()
 		for byte in segment:
@@ -50,7 +52,7 @@ class TCP_Connection_Final(TCP_Connection):
 				byte = byte[0]
 			data_to_send.append(byte)
 
-		#Sends data (if there is any)
+		#Sends data (if there is any).
 		if data_to_send:
 			self._packetize_and_send(self.SND.NXT, PSH=flag, data = data_to_send)
 			self.SND.NXT += len(data_to_send)
